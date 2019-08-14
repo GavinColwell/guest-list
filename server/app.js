4777d5fefa04b875358c3d7cc826a7996da37374
Gavin Colwell <gcolwell828@gmail.com>
2019-08-13 20:35:00 -0400

    added port from environment variable

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
